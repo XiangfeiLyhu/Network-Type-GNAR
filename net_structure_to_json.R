194032556eca86436ba5a2c15e5b7eae898e4694
XiangfeiLyhu <146840430+XiangfeiLyhu@users.noreply.github.com>
2024-08-28 00:23:03 +0100

    Delete net_structure_to_json.R

--- net_structure_to_json.R
+++
@@ -1,5 +0,0 @@
-library(MASS)
-library(jsonlite)
-
-write(toJSON(net_1967[1][[1]]),file="/Users/lvxiangfei/Desktop/Edges_1967.json")
-write(toJSON(net_1967[2][[1]]),file="/Users/lvxiangfei/Desktop/Dists_1967.json")
